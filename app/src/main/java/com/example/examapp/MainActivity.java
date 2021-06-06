47f48fb56fc819605b2b2dc7d4e8f04372498641
MahmoudHowa <82469536+MahmoudHowa@users.noreply.github.com>
2021-06-06 19:21:09 +0300

    test 2

--- app/src/main/java/com/example/examapp/MainActivity.java
+++ app/src/main/java/com/example/examapp/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.examapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db=openOrCreateDatabase("")
     }
 }
---
+++ app/src/main/java/com/example/examapp/Model/Data.java
@@ -0,0 +1,15 @@
+package com.example.examapp.Model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import static android.database.sqlite.SQLiteDatabase.openOrCreateDatabase;
+
+public class Data  {
+
+
+    public Data() {
+
+    }
+
+}
