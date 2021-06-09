38eca19bfe6e8083b6402681f9efd54f86ef302a
MahmoudHowa <82469536+MahmoudHowa@users.noreply.github.com>
2021-06-10 01:08:21 +0300

    9/6/2021  1:07 aM

---
+++ app/src/main/java/com/example/examapp/QuestionAddOrModify.java
@@ -0,0 +1,14 @@
+package com.example.examapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionAddOrModify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_add_or_modify);
+    }
+}
