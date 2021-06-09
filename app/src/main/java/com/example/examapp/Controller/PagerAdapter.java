e0d05d4080508c56730bb8f1cd9959e30281786d
SaleemMaini <saleem.maini4@gmail.com>
2021-06-10 00:30:08 +0300

    6/9/2021 12:30pm

--- app/src/main/java/com/example/examapp/Controller/PagerAdapter.java
+++ app/src/main/java/com/example/examapp/Controller/PagerAdapter.java
@@ -4,41 +4,60 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.examapp.Views.Course1;
 import com.example.examapp.Views.Course2;
 import com.example.examapp.Views.Course3;
 import com.example.examapp.Views.Course4;
 
-public class PagerAdapter extends FragmentPagerAdapter {
-   private int numOfTabs;
+public class PagerAdapter extends FragmentStateAdapter {
 
+    public PagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
 
-    public PagerAdapter(FragmentManager fm, int numOfTabs){
-       super(fm);
-       this.numOfTabs = numOfTabs;
-
-   }
     @NonNull
     @Override
-    public Fragment getItem(int position) {
+    public Fragment createFragment(int position) {
         switch (position) {
-            case 0:
-                return new Course1();
             case 1:
                 return new Course2();
             case 2:
                 return new Course3();
             case 3:
                 return new Course4();
-            default:
-                return null;
         }
-
+        return new Course1();
     }
 
     @Override
-    public int getCount() {
-        return numOfTabs;
+    public int getItemCount() {
+        return 4;
     }
 }
+
+//
+//    @NonNull
+//    @Override
+//    public Fragment getItem(int position) {
+//        switch (position) {
+//            case 0:
+//                return new Course1();
+//            case 1:
+//                return new Course2();
+//            case 2:
+//                return new Course3();
+//            case 3:
+//                return new Course4();
+//            default:
+//                return null;
+//        }
+//
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return numOfTabs;
+//    }
--- app/src/main/java/com/example/examapp/Views/QuestionMangmentActivity.java
+++ app/src/main/java/com/example/examapp/Views/QuestionMangmentActivity.java
@@ -2,12 +2,13 @@ package com.example.examapp.Views;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.examapp.Controller.PagerAdapter;
 import com.example.examapp.R;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
@@ -24,17 +25,17 @@ public class QuestionMangmentActivity extends AppCompatActivity {
         TabItem Course2 = findViewById(R.id.Course2);
         TabItem Course3 = findViewById(R.id.Course3);
         TabItem Course4 = findViewById(R.id.Course4);
-        ViewPager viewPager = findViewById(R.id.viewPager);
-
-        PagerAdapter pagerAdapter = new com.example.examapp.Controller.PagerAdapter(getSupportFragmentManager(),
-                tabLayout.getTabCount());
+        PagerAdapter pagerAdapter;
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        pagerAdapter = new com.example.examapp.Controller.PagerAdapter(getSupportFragmentManager(),
+                getLifecycle());
 
         viewPager.setAdapter(pagerAdapter);
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-            }
+                }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
@@ -46,6 +47,10 @@ public class QuestionMangmentActivity extends AppCompatActivity {
 
             }
         });
-
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));}
+        });
     }
 }
